4d07849494c3febd143d9f964a55db76c38130bf
peradze <peradze.tornike0@gmail.com>
2019-10-02 17:45:11 +0400

    added owners list and updated controller

--- pet-client-web/src/main/java/com/tornikeperadze/petclinic/controllers/OwnerController.java
+++ pet-client-web/src/main/java/com/tornikeperadze/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.tornikeperadze.petclinic.controllers;
 
+import com.tornikeperadze.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwner() {
+    public String listOwner(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
