ca0fb0effd26cb1eac3346d7a8a2fe009a1e2a31
peradze <peradze.tornike0@gmail.com>
2019-10-02 17:16:28 +0400

    adding bootstrap for data

--- pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.tornikeperadze.petclinic.services.map;
 
 import com.tornikeperadze.petclinic.model.Owner;
-import com.tornikeperadze.petclinic.services.CrudService;
+import com.tornikeperadze.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void delete(Owner object) {
         super.delete(object);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/map/PetServiceMap.java
@@ -1,13 +1,13 @@
 package com.tornikeperadze.petclinic.services.map;
 
-import com.tornikeperadze.petclinic.model.Vet;
-import com.tornikeperadze.petclinic.services.VetService;
+import com.tornikeperadze.petclinic.model.Pet;
+import com.tornikeperadze.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
-    public Set<Vet> findAll() {
+    public Set<Pet> findAll() {
         return super.findAll();
     }
 
@@ -17,17 +17,17 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
     }
 
     @Override
-    public void delete(Vet object) {
+    public void delete(Pet object) {
         super.delete(object);
     }
 
     @Override
-    public Vet save(Vet object) {
+    public Pet save(Pet object) {
         return super.save(object.getId(), object);
     }
 
     @Override
-    public Vet findById(Long id) {
+    public Pet findById(Long id) {
         return super.findById(id);
     }
 }
