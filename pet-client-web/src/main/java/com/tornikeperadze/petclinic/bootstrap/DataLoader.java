43ac2ded53616ec091208c601952b0743dedb268
peradze <peradze.tornike0@gmail.com>
2019-10-03 15:52:27 +0400

    added PetTypeService to bootstrap.

--- pet-client-web/src/main/java/com/tornikeperadze/petclinic/bootstrap/DataLoader.java
+++ pet-client-web/src/main/java/com/tornikeperadze/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.tornikeperadze.petclinic.bootstrap;
 
 import com.tornikeperadze.petclinic.model.Owner;
+import com.tornikeperadze.petclinic.model.PetType;
 import com.tornikeperadze.petclinic.model.Vet;
 import com.tornikeperadze.petclinic.services.OwnerService;
+import com.tornikeperadze.petclinic.services.PetTypeService;
 import com.tornikeperadze.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
--- pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/map/PetTypeServiceMap.java
@@ -2,9 +2,11 @@ package com.tornikeperadze.petclinic.services.map;
 
 import com.tornikeperadze.petclinic.model.PetType;
 import com.tornikeperadze.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
