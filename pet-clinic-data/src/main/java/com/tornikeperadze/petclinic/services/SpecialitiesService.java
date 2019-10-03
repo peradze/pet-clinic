aba80771f27ee2a12c2f1feed3f48d00cab1f27b
peradze <peradze.tornike0@gmail.com>
2019-10-03 16:53:13 +0400

    Added Specialities Map Impl.

---
+++ pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.tornikeperadze.petclinic.services;
+
+import com.tornikeperadze.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
