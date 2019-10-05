b11b1f98340f31f7c1520bff4107201a82254a5d
peradze <peradze.tornike0@gmail.com>
2019-10-05 21:06:15 +0400

    added visit service map and jpa impl

---
+++ pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.tornikeperadze.petclinic.services;
+
+import com.tornikeperadze.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/tornikeperadze/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,35 @@
+package com.tornikeperadze.petclinic.services.map;
+
+import com.tornikeperadze.petclinic.model.Visit;
+import com.tornikeperadze.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
